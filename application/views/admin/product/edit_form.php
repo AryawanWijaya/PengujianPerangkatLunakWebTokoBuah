b2108871e9e633dccbeb8e9ae470b5bd1f2ae75a
Aryawan Wijaya <nikolaus.wijaya@si.ukdw.ac.id>
2020-04-14 19:20:41 +0700

    add date expired field

--- application/views/admin/product/edit_form.php
+++ application/views/admin/product/edit_form.php
@@ -77,7 +77,7 @@
 							</div>
 
 							<div class="form-group">
-								<label for="name">Date*</label>
+								<label for="name">Expired Date*</label>
 								 <input class="form-control <?php echo form_error('expiredDate') ? 'is-invalid':'' ?>"
 								 type="date" name="expiredDate" value="<?php echo $product->expiredDate ?>"/>
 								<div class="invalid-feedback">
